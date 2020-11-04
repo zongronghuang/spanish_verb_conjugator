a10a4f74067d4b61bfbe8476536c49ee7ced71ea
zongronghuang <zongrong.h886@gmail.com>
2020-11-04 17:57:35 +0800

    feat: move upper buttons to ConjugationTable

--- src/components/ConjugationTable.vue
+++ src/components/ConjugationTable.vue
@@ -1,62 +1,78 @@
 <template>
-  <div class="d-flex flex-column align-items-center">
-    <h1 class="mt-4">comer</h1>
+  <div class="container mt-4">
+    <div class="row d-flex justify-content-center">
+      <div class="col offset-2 border">
+        <button class="btn btn-info">Add to list</button>
+      </div>
 
-    <small class="mt-1 mb-3">to eat</small>
+      <div class="d-flex flex-column text-center mx-5 col border">
+        <h1>comer</h1>
+        <small>to eat</small>
+      </div>
 
-    <!-- 時態選單 -->
-    <div class="dropdown">
-      <button
-        class="btn btn-primary dropdown-toggle"
-        type="button"
-        id="dropdownMenuButton"
-        data-toggle="dropdown"
-        aria-haspopup="true"
-        aria-expanded="false"
-      >
-        選擇時態
-      </button>
-      <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-        <a class="dropdown-item" href="#">Present indicative</a>
-        <a class="dropdown-item" href="#">Preterite indicative</a>
-        <a class="dropdown-item" href="#">Imperfect indicative</a>
+      <div class="font-weight-bold col border">
+        <span>Hits</span>:
+        <span>100</span>
       </div>
     </div>
 
-    <table class="table w-50 mx-auto mt-3 mb-5">
-      <tbody>
-        <tr class="border">
-          <th scope="row" class="w-50">yo</th>
-          <td>Mark</td>
-        </tr>
-        <tr class="border">
-          <th scope="row" class="w-50">tu</th>
-          <td>Jacob</td>
-        </tr>
-        <tr class="border">
-          <th scope="row" class="w-50">
-            el / ella <br />
-            usted
-          </th>
-          <td>Larry</td>
-        </tr>
-        <tr class="border">
-          <th scope="row" class="w-50">nosotros</th>
-          <td>Larry</td>
-        </tr>
-        <tr class="border">
-          <th scope="row" class="w-50">vosotros</th>
-          <td>Larry</td>
-        </tr>
-        <tr class="border">
-          <th scope="row" class="w-50">
-            ellos/ellas <br />
-            ustedes
-          </th>
-          <td>Larry</td>
-        </tr>
-      </tbody>
-    </table>
+    <!-- 時態選單 -->
+    <div class="row">
+      <div class="dropdown col text-center border">
+        <button
+          class="btn btn-primary dropdown-toggle"
+          type="button"
+          id="dropdownMenuButton"
+          data-toggle="dropdown"
+          aria-haspopup="true"
+          aria-expanded="false"
+        >
+          選擇時態
+        </button>
+        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+          <a class="dropdown-item" href="#">Present indicative</a>
+          <a class="dropdown-item" href="#">Preterite indicative</a>
+          <a class="dropdown-item" href="#">Imperfect indicative</a>
+        </div>
+      </div>
+    </div>
+
+    <div class="row">
+      <table class="table mx-auto mt-3 mb-5 col-6">
+        <tbody>
+          <tr class="border">
+            <th scope="row" class="w-50">yo</th>
+            <td>Mark</td>
+          </tr>
+          <tr class="border">
+            <th scope="row" class="w-50">tu</th>
+            <td>Jacob</td>
+          </tr>
+          <tr class="border">
+            <th scope="row" class="w-50">
+              el / ella <br />
+              usted
+            </th>
+            <td>Larry</td>
+          </tr>
+          <tr class="border">
+            <th scope="row" class="w-50">nosotros</th>
+            <td>Larry</td>
+          </tr>
+          <tr class="border">
+            <th scope="row" class="w-50">vosotros</th>
+            <td>Larry</td>
+          </tr>
+          <tr class="border">
+            <th scope="row" class="w-50">
+              ellos/ellas <br />
+              ustedes
+            </th>
+            <td>Larry</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
 </template>
 
---
+++ src/components/UpperButtons.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="d-flex justify-content-around">
+    <button class="btn btn-info">Add to list</button>
+    <div class="font-weight-bold">
+      <span>Hits</span>:
+      <span>100</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "upper-buttons",
+};
+</script>
+
+<style scoped>
+</style>
