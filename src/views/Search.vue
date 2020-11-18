bebe996da38da4fdb4436d36af7662b18719a6df
zongronghuang <zongrong.h886@gmail.com>
2020-11-18 17:57:44 +0800

    feat: add checkInput function to Search.vue

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -13,12 +13,14 @@
           placeholder="Infinitive (-ar, -er, -ir)"
           aria-label="Feed an infinitive Spanish verb"
           aria-describedby="button-addon2"
+          v-model.trim="input"
         />
         <div class="input-group-append">
           <button
             class="btn btn-outline-secondary font-weight-bold"
             type="button"
             id="button-addon2"
+            @click.prevent.stop="checkInput"
           >
             Conjugate
           </button>
@@ -26,8 +28,12 @@
       </div>
     </div>
     <!-- 錯誤訊息 -->
-    <div class="alert alert-warning w-50 text-center mx-auto" role="alert">
-      A simple warning alert—check it out!
+    <div
+      class="alert alert-warning w-50 text-center mx-auto"
+      role="alert"
+      v-if="alert"
+    >
+      {{ alert }}
     </div>
   </div>
 </template>
@@ -39,6 +45,50 @@ export default {
   name: "search",
   components: {
     ControlPanel,
+  },
+  data() {
+    return {
+      input: "",
+      alert: "",
+    };
+  },
+  methods: {
+    checkInput() {
+      console.log("got the input", this.input, Boolean(this.input));
+      console.log("length", this.input.length);
+      // 不可為空字串
+
+      this.alert = "yayaya";
+      console.log("yaya");
+
+      if (!this.input) {
+        this.alert = "未輸入動詞，無法搜尋";
+      }
+
+      // 不可含數字、特殊符號或空白
+      const regex = /[a-záéíóúü]/;
+      if (this.input.toLowerCase().search(regex)) {
+        this.alert = "不可輸入數字、空白或特殊符號";
+      }
+
+      // 結尾是 arse, erse, irse (提醒拿掉 se 即可)
+      const longEnding = this.input.substring(this.input.length - 4);
+      if (
+        longEnding === "arse" ||
+        longEnding === "erse" ||
+        longEnding === "irse"
+      ) {
+        this.alert = "不可輸入反身動詞；請移除字尾 -se";
+      }
+
+      // 非原形動詞結尾非 -ar, -er, -ir
+      const ending = this.input.substring(this.input.length - 2);
+      if (ending !== "ar" || ending !== "er" || ending !== "ir") {
+        this.alert = "必須輸入原形動詞";
+      }
+
+      // 透過 API 確認輸入的動詞存在
+    },
   },
 };
 </script>
