9df126f5e26aa1caaafab564b6bf26affd034360
Zong-Rong Huang <zongrong.h886@gmail.com>
2020-12-03 01:51:30 +0800

    feat: change .env to .env.local and rewrite env variables for VUE

--- src/apis/dictionary.js
+++ src/apis/dictionary.js
@@ -9,9 +9,10 @@ const axiosInstance = axios.create({
 
   // Oxford dictionaries API 必須提供的 header 資訊
   headers: {
+    "Access-Control-Allow-Origin": "*",
     Accept: responseFormat,
-    app_id: process.env.APP_ID,
-    app_key: process.env.APP_KEY
+    app_id: process.env.VUE_APP_ID,
+    app_key: process.env.VUE_APP_KEY
   }
 })
 
@@ -24,6 +25,8 @@ const dictionary = axiosInstance
 export default {
   // 確認是否存在，是否為動詞
   getLexicalCategory(word) {
+    // console.log('ID', process.env.VUE_APP_ID)
+    // console.log('key', process.env.VUE_APP_KEY)
     return dictionary.get(`/lemmas/es/${word}?strictMatch=${strictMatch}`)
   },
 
