461873bbfc1f1ac26e9784dae0185bd26d55394e
zongronghuang <zongrong.h886@gmail.com>
2020-12-04 18:02:23 +0800

    feat: create databaseAPIs

--- src/apis/database.js
+++ src/apis/database.js
@@ -1,1 +1,66 @@
-// check irregular verbs in the .json file
+import database from '../assets/verb-conjugations.json'
+const data = [...database]
+const infinitives = []
+const conjugationRange = 18
+
+export default {
+  // 取得所有動詞的原形動詞
+  fetchInfinitives() {
+    console.log('Fetching verb infinitives')
+
+
+    for (let i = 0; i < data.length; i = i + 18) {
+
+      const array = data.slice(i, i + 18)
+      console.log('start', i, 'array', array)
+
+
+
+
+      // const index = infinitives.findIndex(ele => ele === data[i].infinitive)
+      // if (index === -1) {
+      //   infinitives.push(data[i].infinitive)
+      // } else {
+      //   // console.log('repeated element', data[i].infinitive, 'id', i)
+      // }
+    }
+
+    console.log('Fetched all verb infinitives')
+    console.log('infinitives length', infinitives.length)
+    console.log(infinitives)
+
+    return infinitives
+  },
+
+  // 取得動詞的所有變化
+  getConjugations(verb) {
+    const index = infinitives.findIndex(infinitive => infinitive === verb)
+
+    if (index === -1) {
+      return console.log('Verb not found')
+    }
+
+    const conjugations = [...data.slice(index, index + conjugationRange)]
+    return conjugations
+  },
+
+  // 取得動詞的中繼資料
+  getMetadata(verb) {
+    const index = infinitives.findIndex(infinitive => infinitive === verb)
+
+    if (index === -1) {
+      return console.log('Verb not found')
+    }
+
+    const metadata = {
+      infinitive: data[index].infinitive,
+      translation: data[index].infinitive_english,
+      engMood: data[index].mood_english,
+      espMood: data[index].mood,
+      engTense: data[index].tense_english,
+      espTense: data[index].tense
+    }
+
+    return metadata
+  }
+}
--- src/views/Search.vue
+++ src/views/Search.vue
@@ -45,7 +45,7 @@
 </template>
 
 <script>
-import dictionaryAPIs from "../apis/dictionary.js";
+import databaseAPIs from "../apis/database.js";
 
 export default {
   name: "search",
@@ -53,50 +53,58 @@ export default {
     return {
       input: "",
       alert: "",
+      infinitives: [],
     };
   },
+  created() {
+    const infinitives = databaseAPIs.fetchInfinitives();
+    this.infinitives = [...infinitives];
+    console.log("App ready!");
+  },
   methods: {
-    async checkInput() {
-      try {
-        const input = this.input.toLowerCase();
-        this.alert = "";
+    checkInput() {
+      const input = this.input.toLowerCase();
+      this.alert = "";
 
-        // 不可為空字串
-        if (!input) {
-          return (this.alert = "未輸入動詞，無法搜尋");
-        }
-
-        // 只接受西班牙文字母 (不可有數字、特殊符號或空白)
-        const regex = /[^a-záéíóúüñ]/;
-        if (input.search(regex) !== -1) {
-          return (this.alert = "不可輸入數字、空白或特殊符號");
-        }
-
-        // 結尾是 arse, erse, irse (提醒拿掉 se 即可)
-        const longEnding = input.substring(input.length - 4);
-        if (
-          longEnding === "arse" ||
-          longEnding === "erse" ||
-          longEnding === "irse"
-        ) {
-          return (this.alert = "請移除結尾 -se");
-        }
-
-        // 非原形動詞結尾非 -ar, -er, -ir
-        const ending = input.substring(input.length - 2);
-        if (ending !== "ar" && ending !== "er" && ending !== "ir") {
-          return (this.alert = "請輸入原形動詞");
-        }
-
-        // 透過 API 確認輸入的動詞存在
-        const data = await dictionaryAPIs.getLexicalCategory(input);
-
-        console.log("lexical category", data);
-        // 轉址到 wordcard 頁面
-        this.$router.push("/wordcard");
-      } catch (error) {
-        return (this.alert = error);
+      // 不可為空字串
+      if (!input) {
+        return (this.alert = "未輸入動詞，無法搜尋");
       }
+
+      // 只接受西班牙文字母 (不可有數字、特殊符號或空白)
+      const regex = /[^a-záéíóúüñ]/;
+      if (input.search(regex) !== -1) {
+        return (this.alert = "不可輸入數字、空白或特殊符號");
+      }
+
+      // 結尾是 arse, erse, irse (提醒拿掉 se 即可)
+      const longEnding = input.substring(input.length - 4);
+      if (
+        longEnding === "arse" ||
+        longEnding === "erse" ||
+        longEnding === "irse"
+      ) {
+        return (this.alert = "請移除結尾 -se");
+      }
+
+      // 非原形動詞結尾非 -ar, -er, -ir
+      const ending = input.substring(input.length - 2);
+      if (ending !== "ar" && ending !== "er" && ending !== "ir") {
+        return (this.alert = "請輸入原形動詞");
+      }
+
+      // 透過 API 確認輸入的動詞存在
+      const index = this.infinitives.findIndex(
+        (infinitive) => infinitive === input
+      );
+      if (index === -1) {
+        return (this.alert = "資料庫內找不到此動詞");
+      }
+
+      // 動詞存在 => 取得動詞的所有變化 + 轉址到 conjugation card 頁面
+      const conjugations = databaseAPIs.getConjugations(input);
+      console.log("conjugations", conjugations);
+      this.$router.push("/conjugation_card");
     },
     collapseAlert() {
       this.alert = "";
