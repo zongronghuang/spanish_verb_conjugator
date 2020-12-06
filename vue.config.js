b1a0bcf5348aad16261abe94971e1fc155b9f08e
Zong-Rong Huang <zongrong.h886@gmail.com>
2020-12-07 01:14:20 +0800

    feat: set up Vue proxy to fetch data from Oxford

--- vue.config.js
+++ vue.config.js
@@ -1,18 +1,18 @@
-// module.exports = {
-//   devServer: {
-//     proxy: {
-//       '/api/v2': {
-//         target: "https://od-api.oxforddictionaries.com",
-//         changeOrigin: true,
-//         secure: true,
-//         ws: true,
-//         logLevel: 'debug',
-//         headers: {
-//           Accept: 'application/json',
-//           app_id: process.env.VUE_APP_ID,
-//           app_key: process.env.VUE_APP_KEY
-//         }
-//       }
-//     }
-//   }
-// }
+module.exports = {
+  devServer: {
+    proxy: {
+      '/api/v2': {
+        target: "https://od-api.oxforddictionaries.com",
+        changeOrigin: true,
+        secure: true,
+        ws: true,
+        logLevel: 'debug',
+        headers: {
+          Accept: 'application/json',
+          app_id: process.env.VUE_APP_ID,
+          app_key: process.env.VUE_APP_KEY
+        }
+      }
+    }
+  }
+}
