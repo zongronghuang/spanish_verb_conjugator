dc424e027dd1ef36ce879bfeafb2382366dc3cb4
zongronghuang <zongrong.h886@gmail.com>
2022-03-10 16:00:26 +0800

    feat: add buttons and InlineSearchBar

--- src/components/modals/SlideMenuModal.vue
+++ src/components/modals/SlideMenuModal.vue
@@ -19,10 +19,19 @@
             <span aria-hidden="true">&times;</span>
           </button>
         </div>
-        <div class="modal-body px-0 py-0">
-          xxxx
+        <div class="modal-body px-0 pb-4 d-flex justify-content-center">
+          <InlineSearchBar />
         </div>
-        <div class="modal-footer py-1 border-0"></div>
+        <div class="modal-footer py-1 mt-5">
+          <div class="mt-2 w-100 d-flex justify-content-around">
+            <IrregularVerbsButton />
+            <ChanceButton/>
+          </div>
+          <div class="mt-4 w-100 d-flex justify-content-around">
+            <ReportButton/>
+            <SettingsButton />
+          </div>
+        </div>
       </div>
     </div>
 
@@ -31,15 +40,30 @@
 
 <script>
 // import $ from "jquery";
+import InlineSearchBar from '../InlineSearchBar.vue'
+import ChanceButton from '../navButtons/ChanceButton.vue'
+import IrregularVerbsButton from '../navButtons/IrregularVerbsButton.vue'
+import ReportButton from '../navButtons/ReportButton.vue'
+import SettingsButton from '../navButtons/SettingsButton.vue'
 
 export default {
   name: 'slide-menu-modal',
   created() {
     console.log('[created] SlideMenu')
+  },
+  components: {
+    InlineSearchBar,
+    ChanceButton,
+    IrregularVerbsButton,
+    ReportButton,
+    SettingsButton
   }
 }
 </script>
 
 <style scoped>
 
+.modal-footer {
+  background-color: var(--spanish-red);
+}
 </style>
